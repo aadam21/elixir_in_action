b88ea331eedd901c04c907451044c4e8768b28b9
Adam Phillips <aadam21@gmail.com>
2020-03-17 17:36:24 -0400

    Abstracting With Structs
    Book work/examples introducing Structs and using them for abstraction

---
+++ lib/chapter_4/simple_todo.ex
@@ -0,0 +1,18 @@
+defmodule TodoList do
+  @moduledoc """
+    Module for handling the data abstraction involved in creating a Todo List
+  """
+
+  @doc """
+    Create a new Todo List, using the MultiDict module
+  """
+  def new(), do: MultiDict.new()
+
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
+  end
+
+  def entries(todo_list, date) do
+    MultiDict.get(todo_list, date)
+  end
+end
